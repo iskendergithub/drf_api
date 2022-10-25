8a2c5351a4ef9350ba546499d4fc5675ddb9cbfb
iskendergithub <iskenmamatov@gmail.com>
2022-10-25 09:45:05 +0600

    new updates

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Product
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CreateProductSerializer
+
+
+class ProductCreateView(APIView):
+    """Creating APIView for product"""
+
+    def post(self, request):
+        product = CreateProductSerializer(data=request.data)
+        if product.is_valid():
+            product.save()
+        return Response(status=201)
+
